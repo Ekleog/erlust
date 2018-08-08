f6e5a21cf4ff10d1729eccf1c08d7d686052f6ae
Léo Gaspard <leo@gaspard.io>
2018-08-09 01:37:43 +0900

    Not-working-yet receive function

--- erlust/src/lib.rs
+++ erlust/src/lib.rs
@@ -7,9 +7,12 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-use futures::{future, sync::mpsc, Future, Sink};
+use futures::{future, sync::mpsc, Future, Sink, Stream};
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, sync::RwLock};
+use std::{
+    collections::{HashMap, LinkedList},
+    sync::RwLock,
+};
 
 const QUEUE_BUFFER: usize = 64; // TODO: (C) fiddle
 
@@ -51,6 +54,7 @@ lazy_static! {
 struct LocalChannel {
     actor_id: ActorId,
     receiver: LocalReceiver,
+    waiting:  LinkedList<LocalMessage>, // TODO: (C) Evaluate whether Vec wouldn't be better
 }
 
 impl LocalChannel {
@@ -58,7 +62,11 @@ impl LocalChannel {
         let (sender, receiver) = mpsc::channel(QUEUE_BUFFER);
         // TODO: (A) make async (qutex + https://github.com/rust-lang-nursery/futures-rs/issues/1187 ?)
         let actor_id = LOCAL_SENDERS.write().unwrap().allocate(sender);
-        LocalChannel { actor_id, receiver }
+        LocalChannel {
+            actor_id,
+            receiver,
+            waiting: LinkedList::new(),
+        }
     }
 }
 
@@ -107,11 +115,46 @@ impl Pid {
 
 pub enum Void {}
 
-pub fn receive(
-    _wanted: &Fn(&Send) -> bool,
-) -> impl Future<Item = LocalMessage, Error = Void> {
-    future::ok(Box::new(()) as LocalMessage) // TODO: (A) implement
+// Do not rely on this function being stable. Despite being `pub`, it is part of
+// the *internal* API of Erlust, that is to be used by the documented `receive`
+// only.
+/*
+#[doc(hidden)]
+pub fn __receive<IgnoreFn, Fut, E>(ignore: IgnoreFn) -> impl Future<Item = LocalMessage, Error = ()>
+where
+    Fut: Future<Item = bool, Error = ()>,
+    IgnoreFn: Fn(&LocalMessage) -> Fut,
+{
+    MY_CHANNEL.with(|c| {
+        // First, attempt to find in waiting list
+        for m in c.waiting {
+            // TODO: (A) Make it await!() when possible
+            if !ignore(&m).wait().unwrap_or(true) {
+                return future::Either::A(future::ok(m));
+            }
+        }
+
+        // Push all irrelevant messages to the waiting list
+        // TODO: (B) Make this await!() when possible
+        future::Either::B(
+            c.receiver
+                .by_ref()
+                .take_while(ignore)
+                .fold(&mut c.waiting, |wait, msg| {
+                    wait.push_back(msg);
+                    future::ok(wait)
+                })
+                .and_then(|_| {
+                    c.receiver
+                    .by_ref()
+                    .into_future()
+                    .map(|(elt, _)| elt.unwrap()) // TODO: (B) Handle end-of-stream?
+                    .map_err(|((), _)| ())
+                }),
+        )
+    })
 }
+*/
 
 #[cfg(test)]
 mod tests {
