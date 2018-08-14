870da9534dc1083871fed607f27e75c229924cee
Léo Gaspard <leo@gaspard.io>
2018-08-15 01:30:19 +0900

    erlust: make LocalMessage Any

--- erlust/src/lib.rs
+++ erlust/src/lib.rs
@@ -17,6 +17,7 @@ extern crate serde_derive;
 use futures::{channel::mpsc, prelude::*};
 use serde::{Deserialize, Serialize};
 use std::{
+    any::Any,
     cell::RefCell,
     collections::{HashMap, LinkedList},
     mem::{self, PinMut},
@@ -29,7 +30,7 @@ const QUEUE_BUFFER: usize = 64; // TODO: (C) fiddle
 // TODO: (B) Limit waiting queue size too
 
 type ActorId = usize;
-type LocalMessage = Box<Send + 'static>;
+type LocalMessage = Box<Any + Send + 'static>;
 type LocalSender = mpsc::Sender<LocalMessage>;
 type LocalReceiver = mpsc::Receiver<LocalMessage>;
 
