d77e42098ace07e3320995b79e71d60e8d7f890f
Léo Gaspard <leo@gaspard.io>
2018-08-18 01:00:06 +0900

    erlust: split receive into separate module

--- erlust/src/lib.rs
+++ erlust/src/lib.rs
@@ -11,18 +11,15 @@ extern crate futures;
 extern crate lazy_static;
 extern crate serde;
 #[cfg(test)]
-#[macro_use]
 extern crate serde_derive;
 
 mod local_channel;
 mod local_channel_updater;
 mod local_senders;
 mod pid;
+mod receive;
 mod spawn;
 mod types;
-
-use futures::prelude::*;
-use std::mem;
 
 use self::{
     local_channel::{LocalChannel, MY_CHANNEL},
@@ -33,80 +30,14 @@ use self::{
 
 pub use futures::{channel::mpsc::SendError, task::SpawnError};
 
-pub use self::{pid::Pid, spawn::spawn, types::Message};
-
-pub enum ReceiveResult<Ret> {
-    Use(Ret),
-    Skip(LocalMessage),
-}
-
-pub async fn receive<HandleFn, Fut, Ret>(handle: HandleFn) -> Ret
-where
-    Fut: Future<Output = ReceiveResult<Ret>>,
-    HandleFn: Fn(LocalMessage) -> Fut,
-{
-    use self::ReceiveResult::*;
-
-    // This `expect` shouldn't trigger, because `LocalChannelUpdater` should always
-    // keep `MY_CHANNEL` task-local. As such, the only moment where it should be
-    // set to `None` is here, and it is restored before the end of this function,
-    // and `__receive` cannot be called inside `__receive`.
-    let mut chan = MY_CHANNEL
-        .with(|c| c.borrow_mut().take())
-        .expect("Called receive inside receive");;
-
-    // First, attempt to find a message in waiting list
-    for i in 0..chan.waiting.len() {
-        // TODO: (C) consider unsafe here to remove the allocation, dep. on benchmarks
-        let mut msg = Box::new(()) as LocalMessage;
-        mem::swap(&mut msg, &mut chan.waiting[i]);
-        match await!(handle(msg)) {
-            Use(ret) => {
-                chan.waiting.remove(i);
-                MY_CHANNEL.with(|c| *c.borrow_mut() = Some(chan));
-                return ret;
-            }
-            Skip(msg) => {
-                chan.waiting[i] = msg;
-            }
-        }
-    }
-
-    // Push all irrelevant messages to the waiting list, then return relevant one
-    loop {
-        // This `expect` shouldn't trigger, because `chan.receiver.next()` is
-        // supposed to answer `None` iff all `Sender`s associated to the channel
-        // have been dropped. Except we always keep a `Sender` alive in the
-        // `LOCAL_SENDERS` map, and `__receive` should not be able to be called
-        // once the actor has been dropped, so this should be safe.
-        let msg = await!(chan.receiver.next()).expect("Called receive after the actor was dropped");
-        match await!(handle(msg)) {
-            Use(ret) => {
-                MY_CHANNEL.with(|c| *c.borrow_mut() = Some(chan));
-                return ret;
-            }
-            Skip(msg) => {
-                chan.waiting.push_back(msg);
-            }
-        }
-    }
-}
+pub use self::{
+    pid::Pid,
+    receive::{receive, ReceiveResult},
+    spawn::spawn,
+    types::Message,
+};
 
 // TODO: (A) add a registry to record name<->Pid associations?
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[derive(Serialize, Deserialize)]
-    struct TestMsg {
-        foo: String,
-    }
-    impl Message for TestMsg {
-        fn tag() -> &'static str {
-            "test"
-        }
-    }
-
-    // TODO: (A) Add receive! / receive_box! tests
-}
+// TODO: (A) document all the things
+// TODO: (A) test all the things
---
+++ erlust/src/receive.rs
@@ -0,0 +1,61 @@
+use futures::{Future, StreamExt};
+use std::mem;
+
+use crate::{LocalMessage, MY_CHANNEL};
+
+pub enum ReceiveResult<Ret> {
+    Use(Ret),
+    Skip(LocalMessage),
+}
+
+pub async fn receive<HandleFn, Fut, Ret>(handle: HandleFn) -> Ret
+where
+    Fut: Future<Output = ReceiveResult<Ret>>,
+    HandleFn: Fn(LocalMessage) -> Fut,
+{
+    use self::ReceiveResult::*;
+
+    // This `expect` shouldn't trigger, because `LocalChannelUpdater` should always
+    // keep `MY_CHANNEL` task-local. As such, the only moment where it should be
+    // set to `None` is here, and it is restored before the end of this function,
+    // and `__receive` cannot be called inside `__receive`.
+    let mut chan = MY_CHANNEL
+        .with(|c| c.borrow_mut().take())
+        .expect("Called receive inside receive");;
+
+    // First, attempt to find a message in waiting list
+    for i in 0..chan.waiting.len() {
+        // TODO: (C) consider unsafe here to remove the allocation, dep. on benchmarks
+        let mut msg = Box::new(()) as LocalMessage;
+        mem::swap(&mut msg, &mut chan.waiting[i]);
+        match await!(handle(msg)) {
+            Use(ret) => {
+                chan.waiting.remove(i);
+                MY_CHANNEL.with(|c| *c.borrow_mut() = Some(chan));
+                return ret;
+            }
+            Skip(msg) => {
+                chan.waiting[i] = msg;
+            }
+        }
+    }
+
+    // Push all irrelevant messages to the waiting list, then return relevant one
+    loop {
+        // This `expect` shouldn't trigger, because `chan.receiver.next()` is
+        // supposed to answer `None` iff all `Sender`s associated to the channel
+        // have been dropped. Except we always keep a `Sender` alive in the
+        // `LOCAL_SENDERS` map, and `__receive` should not be able to be called
+        // once the actor has been dropped, so this should be safe.
+        let msg = await!(chan.receiver.next()).expect("Called receive after the actor was dropped");
+        match await!(handle(msg)) {
+            Use(ret) => {
+                MY_CHANNEL.with(|c| *c.borrow_mut() = Some(chan));
+                return ret;
+            }
+            Skip(msg) => {
+                chan.waiting.push_back(msg);
+            }
+        }
+    }
+}
