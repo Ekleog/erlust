337f13e1cb6385baede3350d73e7edb238f19b7e
Léo Gaspard <leo@gaspard.io>
2018-08-21 00:30:27 +0900

    general: bump nightly

--- common.nix
+++ common.nix
@@ -19,7 +19,7 @@ rec {
     overlays = [ rustOverlay ];
   };
   rustNightlyChannel = pkgs.rustChannelOf {
-    date = "2018-08-16";
+    date = "2018-08-19";
     channel = "nightly";
   };
   #rustBetaChannel = pkgs.rustChannelOf {
