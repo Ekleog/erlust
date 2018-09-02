618965ec7e4c2f23a10538df58a531a9848d3d1d
Léo Gaspard <leo@gaspard.io>
2018-09-03 02:33:00 +0900

    erlust: document theater

--- erlust/src/theater.rs
+++ erlust/src/theater.rs
@@ -5,19 +5,68 @@ use std::cell::RefCell;
 
 use crate::types::{ActorId, Message, MessageBox};
 
+/// A bunch of actors and functions used for theaters to communicate between
+/// them
 // TODO: (A) provide an implementation of Theater
 pub trait Theater: Message + Clone {
+    /// Returns the local theater, as seen from the theater defined by `self`
+    ///
+    /// For instance, if the link between the local theater and `self` is a
+    /// named pipe, then `here` should return a theater that points to the
+    /// part of the pipe that should be used by the remote end to connect
+    /// to the local end.
+    ///
+    /// If the link between the local theater and `self` is
+    /// TCP-on-a-flat-network, then this function should return a
+    /// theater that points to the address and port on which the local
+    /// process is currently listening.
     fn here(&mut self) -> Box<Self>;
 
-    // A TheaterBox that can be used by the one on the other side of this Theater
-    // to contact the TheaterBox o that can be contacted locally
-    fn sees_as(&mut self, o: Box<dyn TheaterBox>) -> Box<Self>;
+    /// Returns an instance of `Self` that can be used by the remote end of
+    /// this connection (ie. `self`) to communicate with `other` (which is
+    /// defined by a [`TheaterBox`] reachable from the current theater)
+    ///
+    /// For instance, for a TCP-on-a-flat-network connection model, this would
+    /// simply be a no-op (only downcasting `other` into an instance of the
+    /// local theater).
+    ///
+    /// However, things get fun when handling communicating protocols, like a
+    /// protocol for making multiple processes on the same machine and
+    /// another protocol for making multiple machines communicate, each
+    /// optimized to handle only their use case. In this case (ie. if `self`
+    /// cannot actually directly talk with `other`), it may become necessary to
+    /// setup a “bouncer” actor in the local theater, and then return a
+    /// pointer to said local actor, which would then be charged with
+    /// relaying messages to `other`
+    // TODO: (A) this should take an actor and return an actor (cf. doc above)
+    fn sees_as(&mut self, other: Box<dyn TheaterBox>) -> Box<Self>;
 
-    // TODO: (B) return associated type
+    /// Returns a serializer to be used for generating the `msg` argument of
+    /// [`send`] out of a [`Message`]
+    ///
+    /// `out` will be passed to [`send`] after serialization of said message.
+    // TODO: (B) return associated type?
     fn serializer(&mut self, out: &mut Vec<u8>) -> Box<Serializer>;
+
+    /// Returns a deserializer to be used for deserializing the `msg` argument
+    /// from [`inject`]
+    ///
+    /// Usually, this will be the operation opposite to the one [`serializer`]
+    /// performed.
+    // TODO: (B) return associated type?
     fn deserializer<'de>(&mut self, inp: &'de Vec<u8>) -> Box<Deserializer<'de>>;
 
+    /// Send a message to `self`
+    ///
+    /// This should trigger a call to [`inject`] in the theater designed by
+    /// `self`,  with the same arguments as the ones passed to this
+    /// function, plus a `from_theater` equivalent to `self.here()`.
+    ///
+    /// Please note however that the security model assumes that the
+    /// *receiving* side identifies the `from_theater`, not the *sending* side,
+    /// so serializing `self.here()` would most likely be a bad idea.
     // TODO: (B) return impl Trait h:impl-trait-in-trait
+    // TODO: (A) make `tag` a `String`
     fn send(
         &mut self,
         from: ActorId,
@@ -27,20 +76,34 @@ pub trait Theater: Message + Clone {
     ) -> FutureObj<Result<(), failure::Error>>;
 }
 
+/// A [`Box`]-able [`Theater`]
+///
+/// This trait is automatically implemented for all traits implementing
+/// [`Theater`].
 pub trait TheaterBox: MessageBox {
+    /// See [`Theater::here`]
     fn here(&mut self) -> Box<dyn TheaterBox>;
 
+    /// Clones `self` into a [`Box`]
     fn clone_to_box(&self) -> Box<dyn TheaterBox>;
+
+    /// Deserializes from `inp` into the type of `Self`, in a type-erased way
+    /// (ie. into a trait object)
     fn deserialize_as_self(
         &self,
         inp: &mut Deserializer,
     ) -> Result<Box<TheaterBox>, erased_serde::Error>;
 
-    fn sees_as(&mut self, o: Box<dyn TheaterBox>) -> Box<dyn TheaterBox>;
+    /// See [`Theater::sees_as`]
+    fn sees_as(&mut self, other: Box<dyn TheaterBox>) -> Box<dyn TheaterBox>;
 
+    /// See [`Theater::serializer`]
     fn serializer(&mut self, out: &mut Vec<u8>) -> Box<Serializer>;
+
+    /// See [`Theater::deserializer`]
     fn deserializer<'de>(&mut self, inp: &'de Vec<u8>) -> Box<Deserializer<'de>>;
 
+    /// See [`Theater::send`]
     fn send(
         &mut self,
         from: ActorId,
@@ -52,6 +115,10 @@ pub trait TheaterBox: MessageBox {
 
 // TODO: (B) use scoped_tls
 thread_local! {
+    /// The local theater, defined in relation to the remote theater currently being used
+    ///
+    /// This is designed to handle correctly cases of communicating protocols with a gateway that handles multiple
+    /// protocols, and thus has a different address on each protocol.
     pub static HERE: RefCell<Option<Box<TheaterBox>>> = RefCell::new(None);
 }
 
